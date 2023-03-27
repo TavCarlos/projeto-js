31cd8c17a4366b33e67bf0b74c9b15f327c566f2
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-26 21:14:23 -0300

    update

--- movie-project/js/script.js
+++ movie-project/js/script.js
@@ -4,6 +4,8 @@
 //DOM
     const movieSearchBox = document.querySelector('#id-movie-search')
     const submitBox = document.querySelector('#search-movie')
+    const gerericInfo = document.querySelector('#generic-info')
+    const poster = document.querySelector('#movie-poster')
 
     
 
@@ -20,11 +22,27 @@
     async function apifunction(opt){
         let response = await fetch (`https://www.omdbapi.com/?s=${movieSearchBox.value.trim()}&page=1&apikey=618a1d4d`, opt)
         let data = await response.json()
-        findMovie(data.Search)
+        
+       poster.innerHTML = `<img src="${data.Search[0].Poster}" alt="movie poster">`
+       
+       findMovie(data.Search)
     }
 
     function findMovie(colecao){
-        for(let pos in colecao){
-            console.log(colecao[pos].imdbID)
-        }
+            movieID = colecao[0].imdbID
+
+        outputMovie(movieID)
     }
+
+    async function outputMovie(movie){
+        let url = `https://www.omdbapi.com/?i=${movie}&apikey=618a1d4d`
+        let response = await fetch (url)
+        let data = await response.json()
+        
+        gerericInfo.innerHTML = `<p><span>Year: </span> ${data.Year}</p>
+        <p><span>Rated:</span> ${data.Rated}</p>
+        <p> <span>Released:</span> ${data.Released} </p>
+        <p> <span>Runtime:</span>${data.Runtime}</p>
+        `
+        
+    } 
