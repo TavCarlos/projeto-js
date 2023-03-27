d5a36f75137a8421fcfadd1385fe982b7e5aa765
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-27 18:37:34 -0300

    Update

--- movie-project/js/script.js
+++ movie-project/js/script.js
@@ -1,47 +1,66 @@
-//const apiKey = https://www.omdbapi.com/?t=lord&page=1&apikey=618a1d4d
+//const apiKey = https://www.omdbapi.com/?t=lord&page=1&apikey=618a1d4d;
 
-//DOM
-    const movieSearchBox = document.querySelector('#id-movie-search')
-    const submitBox = document.querySelector('#search-movie')
-    const gerericInfo = document.querySelector('#generic-info')
-    const poster = document.querySelector('#movie-poster')
+let Btn = document.querySelector('#search-movie');
+let MovieNameRef = document.querySelector('#movietxt')
+let output = document.querySelector('div#container');
 
-    
+//DOM - elements
+let erro = document.querySelector('#err');
 
-    submitBox.addEventListener('click', (e)=>{
-        const options = {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'default'
-        }
+let poster = document.querySelector('#poster-img');
+let title = document.querySelector('#title');
+let year = document.querySelector('#year');
+let rated = document.querySelector('#rated');
+let released = document.querySelector('#released');
 
-        apifunction(options)
-    })
+let genre = document.querySelector('#genre');
+let director = document.querySelector('#director');
+let writer = document.querySelector('#writer');
+let actor = document.querySelector('#actor');
 
-    async function apifunction(opt){
-        let response = await fetch (`https://www.omdbapi.com/?s=${movieSearchBox.value.trim()}&page=1&apikey=618a1d4d`, opt)
-        let data = await response.json()
-        
-       poster.innerHTML = `<img src="${data.Search[0].Poster}" alt="movie poster">`
-       
-       findMovie(data.Search)
+let plot = document.querySelector('#plot');
+let language = document.querySelector('#language');
+
+
+//let teste = document.querySelector('#teste') //testando para update
+
+//teste.innerHTML = `<img src="guardian-galaxy.jpg">` //testando para update
+
+//function
+let GetMovie = () =>{
+  //  teste.innerHTML = "" //testando para update
+    let movieName = MovieNameRef.value//MovieNameRef.value
+    let url = `https://www.omdbapi.com/?t=${movieName}&page=1&apikey=618a1d4d`
+
+    if(movieName.length == 0){
+        window.alert(`Filme não encontrado. Por favor, tente novamente!`)
+    } else {
+    fetch(url)
+    .then(response => response.json())
+        .then(data =>{
+            if(data.Title == undefined){
+                window.alert(`Não foi possivel encontrar: ${movieName}!`);
+            } else{
+                if(data.Poster == 'N/A'){
+                    poster.innerHTML = `<img src="404poster.jpg" alt="movie-poster">`
+                }else{
+                    poster.innerHTML = `<img src="${data.Poster}" alt="movie-poster">`
+                }
+                title.innerHTML = `${data.Title}`
+                year.innerHTML = `<span>Year:</span>${data.Year}`
+                rated.innerHTML = `<span>Rated:</span>${data.Rated}`
+                released.innerHTML = `<span>Released:</span>${data.Released}`
+
+                genre.innerHTML = `<span>Genre:</span>${data.Genre}</p>`
+                director.innerHTML = `<p id="director"> <span>Director:</span>${data.Director}`
+                writer.innerHTML = `<span>Writer:</span>${data.Writer}`
+                actor.innerHTML = `<span>Actors:</span>${data.Actors}`
+
+                plot.innerHTML = `<span>Plot: </span>${data.Plot}`
+                language.innerHTML = `<span>Language:</span>${data.Language}`
+            }
+        })
     }
+}
 
-    function findMovie(colecao){
-            movieID = colecao[0].imdbID
-
-        outputMovie(movieID)
-    }
-
-    async function outputMovie(movie){
-        let url = `https://www.omdbapi.com/?i=${movie}&apikey=618a1d4d`
-        let response = await fetch (url)
-        let data = await response.json()
-        
-        gerericInfo.innerHTML = `<p><span>Year: </span> ${data.Year}</p>
-        <p><span>Rated:</span> ${data.Rated}</p>
-        <p> <span>Released:</span> ${data.Released} </p>
-        <p> <span>Runtime:</span>${data.Runtime}</p>
-        `
-        
-    } 
+Btn.addEventListener('click', GetMovie)
