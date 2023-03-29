e5486cc6e9b87a0e3494dace18c0b019c99aeda1
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-29 12:28:37 -0300

    Update

--- movie-search-project/js/script.js
+++ movie-search-project/js/script.js
@@ -1,60 +1,78 @@
 //DOM
-let Btn = document.querySelector('#search-movie');
-let MovieNameRef = document.querySelector('#movietxt')
-let output = document.querySelector('div#container');
-let result = document.querySelector('div#result');
+const Btn = document.querySelector('#search-movie');
+const MovieNameRef = document.querySelector('#movietxt')
+const result = document.querySelector('div#result');
 
 
-//function
-let GetMovie = () =>{
-    let movieName = MovieNameRef.value
-    let url = `https://www.omdbapi.com/?t=${movieName}&page=1&apikey=618a1d4d`
+//functions
+const GetMovie = (movieName) =>{
+    const url = `https://www.omdbapi.com/?t=${movieName}&page=1&apikey=618a1d4d`
 
-    if(movieName.length == 0){
-        result.innerHTML = `<h1 class="movie-not-found">Movie not Found!</h1>`
-    } else {
-
-    //buscando dados da API
+    //getting data from API
     fetch(url)
     .then(response => response.json())
         .then(data =>{
-            if(data.Title == undefined){ //se o dado vir como undefined
-                result.innerHTML = `<h1 class="movie-not-found">Movie not Found!</h1>`
-            }else{
-                result.innerHTML = `
-                <div id="movie-info">
-                    <div id="poster-photo">
-                        <img src="${data.Poster}" onerror="this.src='404poster.jpg'" alt="">
-                    </div>
-                         <div id="details">
-                        <div id="title">
-                            <h3>${data.Title}</h3>
-                        </div>
-                        <span>Year:</span>${data.Year}
-                        <span>Rated:</span>${data.Rated}
-                        <span>Released:</span>${data.Released}
-                        <div id="genre">
-                            <span>Genre: </span>${data.Genre}
-                        </div>
-                        <p><span>Director: </span>${data.Director}</p>
-                        <p><span>Writer: </span>${data.Writer}</p>
-                        <p><span>Actor: </span>${data.Actors}</p>
-                        <div id="plot">
-                            <span>Plot: </span>${data.Plot}
-                        </div>
-                        <span>Language: </span>${data.Language}
-                    </div>
-                </div>`
-            } 
+                if(data.Title === undefined){ //checking some errors
+                    result.innerHTML = `<h1 class="movie-not-found">Movie not Found!</h1>`
+                } else{
+                    const posterUrl = data.Poster === 'N/A'? '404poster.jpg' : data.Poster
+                    renderMovieInfo(data, posterUrl)
+                }
+            })
+        .catch(error =>{
+            console.error(error)
+            result.innerHTML = `<h1 class="movie-not-found">Error connecting to the API</h1>`
         })
-        
-    } 
-}
+    }
 
-Btn.addEventListener('click', GetMovie)
-//Pesquisando ao pressionar ENTER
-MovieNameRef.addEventListener('keypress', function(e){
+//Adding data to the movie info on screen
+const renderMovieInfo = (data, posterUrl) =>{
+        result.innerHTML = `
+        <div id="movie-info">
+            <div id="poster-photo">
+                <img src="${posterUrl}">
+            </div>
+                 <div id="details">
+                <div id="title">
+                    <h3>${data.Title}</h3>
+                </div>
+                <span>Year:</span>${data.Year}
+                <span>Rated:</span>${data.Rated}
+                <span>Released:</span>${data.Released}
+                <div id="genre">
+                    <span>Genre: </span>${data.Genre}
+                </div>
+                <p><span>Director: </span>${data.Director}</p>
+                <p><span>Writer: </span>${data.Writer}</p>
+                <p><span>Actor: </span>${data.Actors}</p>
+                <div id="plot">
+                    <span>Plot: </span>${data.Plot}
+                </div>
+                <span>Language: </span>${data.Language}
+            </div>
+        </div>`
+    }
+
+
+//Event Listeners
+Btn.addEventListener('click', () => {
+    const movieName = MovieNameRef.value.trim() //remove all blanks in front of or at the end of a string
+    if(movieName){
+        GetMovie(movieName)
+    } else{
+        result.innerHTML = `<h1 class="movie-not-found">Movie name is required!</h1>`
+    }
+})
+
+
+//Event listener ENTER
+MovieNameRef.addEventListener('keypress', (e) => {
     if (e.keyCode === 13){
-        GetMovie()
+        const movieName = MovieNameRef.value.trim()
+        if(movieName){
+            GetMovie(movieName)
+        } else{
+            result.innerHTML = `<h1 class="movie-not-found">Movie name is required!</h1>`
+        }
     }
 })
